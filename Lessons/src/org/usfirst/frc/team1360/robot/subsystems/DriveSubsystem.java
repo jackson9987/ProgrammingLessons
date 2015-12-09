348737c058965986eed80242173b9e04a472e588
Greg Jackson <jackson.g.87@gmail.com>
2015-12-08 22:30:37 -0500

    Update DriveSubsystem.java
    Added reference to the Jaguar lib
    Added missing semicolons on lines 13 and 14
    Added reference to class itself (i.e. this) in the drive function

--- Lessons/src/org/usfirst/frc/team1360/robot/subsystems/DriveSubsystem.java
+++ Lessons/src/org/usfirst/frc/team1360/robot/subsystems/DriveSubsystem.java
@@ -2,17 +2,17 @@
 package org.usfirst.frc.team1360.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-
+import edu.wpi.first.wpilibj.Jaguar;
 /**
  *
  */
 public class DriveSubsystem extends Subsystem
 {
     
-    Jaguar left = new Jaguar(RobotMap.DRIVE_LEFT);
-    Jaguar left2 = new Jaguar(RobotMap.DRIVE_LEFT)
-    Jaguar right = new Jaguar(RobotMap.DRIVE_LEFT)
-	Jaguar right2 = new Jaguar(RobotMap.DRIVE_LEFT);
+    Jaguar left = new Jaguar(RobotMap.DRIVE_LEFT_1);
+    Jaguar left2 = new Jaguar(RobotMap.DRIVE_LEFT_1);
+    Jaguar right = new Jaguar(RobotMap.DRIVE_LEFT_1);
+    Jaguar right2 = new Jaguar(RobotMap.DRIVE_LEFT_1);
 
     public void initDefaultCommand()
     {
@@ -20,10 +20,10 @@ public class DriveSubsystem extends Subsystem
 
     public boolean drive(float left, float right)
     {
-    	this.left.set(-left)
-    	left2.set(-left)
-    	right.set(right);
-    	right2.set(right);
+    	this.left.set(-left);
+    	this.left2.set(-left);
+    	this.right.set(right);
+    	this.right2.set(right);
     }
 }
 
