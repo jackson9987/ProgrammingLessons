132fac308e6d9d02845313fecff7ba61cce5cb78
Greg Jackson <jackson.g.87@gmail.com>
2015-12-08 22:40:08 -0500

    Update OI.java
    Added reference to Joystick lib
    added static to Joystick declaration in line 13
    changed argument in getAxis function from 0 to null on line 14 to match function argument type

--- Lessons/src/org/usfirst/frc/team1360/robot/OI.java
+++ Lessons/src/org/usfirst/frc/team1360/robot/OI.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1360.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import org.usfirst.frc.team1360.robot.commands.ExampleCommand;
 
@@ -9,7 +10,7 @@ import org.usfirst.frc.team1360.robot.commands.ExampleCommand;
  */
 public class OI 
 {
-    Joystick drive = new Joystick(0);
-    public static float drive_LeftStickX = drive.getAxis(0);
+    static Joystick drive = new Joystick(0);
+    public static float drive_LeftStickX = drive.getAxis(null);
 }
 
